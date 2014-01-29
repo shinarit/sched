44753d10dc3dc135bde1c26b787a5035a688bf56
tetra <tetra666@gmail.com>
2014-01-29 20:29:22 +0100

    resource reader now adds resources to resource pool

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <mutex>
 
 #include <fstream>
 
@@ -10,25 +11,48 @@
 #include <cstdlib>
 #include <ctime>
 
-void receiveResources(const char* resFile) 
+
+//(nodeID, (all, free))
+typedef std::map<int, std::pair<int, int>> ResourceMonitor;
+
+std::mutex ResourceMutex;
+
+void printResourceMonitor(const ResourceMonitor& resMon);
+
+void receiveResources(const char* resFile, ResourceMonitor& resMon)
 {
   std::ifstream in(resFile);
   int nodeId;
   while (in >> nodeId)
   {
     std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 10 * 100));
-    std::cout << nodeId << '\n';
+//    std::cout << nodeId << '\n';
+
+    ResourceMutex.lock();
+
+    ++resMon[nodeId].first;
+    ++resMon[nodeId].second;
+
+    printResourceMonitor(resMon);
+    std::cout << '\n';
+
+    ResourceMutex.unlock();
   }
 }
 
-//(nodeID, (all, free))
-typedef std::map<int, std::pair<int, int>> ResourceMonitor;
+void printResourceMonitor(const ResourceMonitor& resMon)
+{
+  for (const auto& resPair : resMon)
+  {
+    std::cout << resPair.first << ": " << resPair.second.second << " / " << resPair.second.first << '\n';
+  }
+}
 
 int main(int argc, char* argv[]) 
 {
   std::srand(std::time(0));
   ResourceMonitor resMon;
 
-  std::thread first(receiveResources, argv[1]);
+  std::thread first(receiveResources, argv[1], resMon);
   first.join();                // pauses until first finishes
 }
